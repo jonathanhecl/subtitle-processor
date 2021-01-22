74385889f53cddbaa6a355318f535e03a05baa4c
jonathanhecl <jonathanhecl@gmail.com>
2021-01-22 01:27:18 -0300

    Nothing

--- subtitles/subtitle.go
+++ subtitles/subtitle.go
@@ -1,9 +1,11 @@
 package subtitles
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/jonathanhecl/subtitle-processor/subtitles/format"
 	"github.com/jonathanhecl/subtitle-processor/subtitles/models"
@@ -18,12 +20,15 @@ type Subtitle struct {
 func (sub *Subtitle) LoadFilename(filename string) (err error) {
 	sub.Filename = filename
 
-	data, err := ioutil.ReadFile(filename)
+	start := time.Now()
+
+	raw, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
-	content := string(data)
+	content := string(raw)
+
 	content = strings.Replace(content, "\r\n", "\n", -1) // standardize line break
 	content += "\n\n"                                    // lastest line break
 	ret, err := format.ReadSRT(content)                  // check if it is the SRT format
@@ -38,6 +43,8 @@ func (sub *Subtitle) LoadFilename(filename string) (err error) {
 			sub.Lines = ret
 		}
 	}
+
+	fmt.Println("Processed in ", time.Since(start).String())
 
 	return err
 }
